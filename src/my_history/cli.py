cfaae650cb1fdc5148b415911324292de4781b2b
DONGUK777 <kimdongyork@gmail.com>
2024-07-24 09:23:08 +0900

    수정

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import sys
 
-def cnt(ind):
+def cnt():
+    ind = sys.argv[1]
     df = pd.read_parquet("~/tmp/history.parquet")
     fdf = df[df['cmd'].str.contains(ind)]
     cnt = fdf['cnt'].sum()
     print(cnt)
 
-def main():
-    ind = sys.argv[1]
-    cnt(ind)
 
-if __name__=='__main__':
-    main()
 
 
